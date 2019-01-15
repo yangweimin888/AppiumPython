b5cbae50ed59d552d571fd61419e6b0fe0d3312e
yangweimin888 <1148479571@qq.com>
2019-01-15 10:25:30 +0800

    修复压缩文件时重复的问题

--- common/zip_files.py
+++ common/zip_files.py
@@ -20,26 +20,24 @@ class ZipMethod(object):
         zip_file = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
         for file in data:
             self.log.info('%s正在压缩中......' % file)
-            zip_file.write(file)
+            zip_file.write(os.path.join(self.file_path, file))
         zip_file.close()
         self.log.info('压缩完成')
 
 
-    def zip_path(self, zip_name):
-        """按照全路径压缩文件"""
+    def zip_path(self, zip_name, zip_file):
+        """指定具体文件按照全路径进行压缩"""
         data = os.listdir(self.file_path)
         if len(data) == None or len(data) < 1:
-            self.log.info('待压缩的文件目录：%s' % self.file_path + '里面不存在文件，不需要压缩')
+            self.log.warning('待压缩的文件目录：%s' % self.file_path + '里面不存在文件，不需要压缩')
         else:
-            zip_file = zipfile.ZipFile(self.file_path, 'w', zipfile.ZIP_DEFLATED)
-            for file in data:
-                self.log.info('%s正在压缩中......' % file)
-                filefullpath = os.path.join(self.file_path, file)
-                zip_file.write(filefullpath, file)
-            zip_file.close()
-
-
-
+            zip = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
+            if zip_file not in data:
+                self.log.info('您所要压缩的文件不存在，请再次确认！')
+            else:
+                zip.write(os.path.join(self.file_path, zip_file))
+                self.log.info('压缩完成')
+            zip.close()
 
 
 
